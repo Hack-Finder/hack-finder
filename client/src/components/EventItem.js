027761afa32b52f14504080e006f1355626ba5c4
Florian Poschlod <florian.poschlod@mailbox.org>
2021-03-06 13:49:36 +0100

    Changes layout of event item, adds event picture

--- client/src/components/EventItem.js
+++ client/src/components/EventItem.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function EventItem(props) {
 
-  console.log(props.event)
-
   return (
-    <div className="flex flex-row border-b border-gray-300 p-8">
+    // TODO: Link to EventDetail component
+    <Link to="" key={props.event._id} className="flex flex-row border-b border-gray-300 p-8">
 
       <div className="mr-12">
-        <p>{props.event.date}</p>
+        <img src={props.event.img.imgPath} alt={props.event.img.imgName} style={{ maxWidth: '200px', maxHeight: '120px' }} />
       </div>
 
       <div>
         <h2 className="font-bold text-2xl mb-4">{props.event.title}</h2>
-        <p>{props.event.description}</p>
+
+        <div className="flex flex-row flex-wrap mb-2" >
+          <p className="mr-4 mb-1">📌 {props.event.location}</p>
+          <p className="mr-4 mb-1">⏰ {props.event.deadlineA}</p>
+          <p className="mr-4 mb-1">🏆 {props.event.rewardTag[0]}</p>
+        </div>
+
+        <p>{props.event.description.slice(0, 120)}...</p>
       </div>
 
-    </div>
+    </Link>
 
   )
 }
