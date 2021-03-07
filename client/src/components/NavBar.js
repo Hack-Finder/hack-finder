6a6f6ae995ed9ee5f5060aad601d844dce38bb53
Florian Poschlod <florian.poschlod@mailbox.org>
2021-03-07 12:30:42 +0100

    Connects login and sign up links to routes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -58,8 +58,8 @@ export default class NavBar extends React.Component {
               </div>
               <div className="hidden sm:block sm:ml-6">
                 <div className="flex space-x-4">
-                  <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-                  <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                  <Link to="/signup" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign up</Link>
+                  <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in</Link>
                 </div>
               </div>
             </div>
