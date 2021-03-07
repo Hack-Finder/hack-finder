6f0aad1c2301f166c9f1788affd6d48209306c39
Florian Poschlod <florian.poschlod@mailbox.org>
2021-03-07 12:12:44 +0100

    Adds comments to clarify navBar dropdown code structure

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -34,23 +34,19 @@ export default class NavBar extends React.Component {
               {/* <!-- Mobile menu button--> */}
               <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
-                {/* <!-- */}
+
                 {/* Icon when menu is closed. */}
-
                 {/* Heroicon name: outline/menu */}
-
                 {/* Menu open: "hidden", Menu closed: "block" */}
-                {/* --> */}
+
                 <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
-                {/* <!-- */}
+
                 {/* Icon when menu is open. */}
-
                 {/* Heroicon name: outline/x */}
-
                 {/* Menu open: "block", Menu closed: "hidden" */}
-                {/* --> */}
+
                 <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
@@ -71,6 +67,7 @@ export default class NavBar extends React.Component {
                 </div>
               </div>
             </div>
+
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                 <span className="sr-only">View notifications</span>
@@ -82,13 +79,13 @@ export default class NavBar extends React.Component {
 
               {/* <!-- Profile dropdown --> */}
               <div className="ml-3 relative">
+                {/* Dopdown menu button (user profile picture) */}
                 <div>
                   <button onClick={this.showModal} type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-expanded="false" aria-haspopup="true">
                     <span className="sr-only">Open user menu</span>
                     <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                   </button>
                 </div>
-
 
                 {/* Dropdown menu, show/hide based on menu state. */}
                 {/* Entering: "transition ease-out duration-100" */}
@@ -97,6 +94,8 @@ export default class NavBar extends React.Component {
                 {/* Leaving: "transition ease-in duration-75" */}
                 {/* From: "transform opacity-100 scale-100" */}
                 {/* To: "transform opacity-0 scale-95" */}
+
+                {/* Actual profile dropdown menu */}
                 {this.state.showModal &&
                   <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
@@ -104,8 +103,8 @@ export default class NavBar extends React.Component {
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
                   </div>
                 }
-
               </div>
+
             </div>
           </div>
         </div>
