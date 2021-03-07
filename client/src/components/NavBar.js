8e9fef51c895825c5eb73077879d8521930e7a9d
Florian Poschlod <florian.poschlod@mailbox.org>
2021-03-07 12:54:49 +0100

    Aligns link group to the right

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -51,57 +51,64 @@ export default class NavBar extends React.Component {
               </button>
             </div>
 
-            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+              {/* Logo */}
               <div className="flex-shrink-0 flex items-center">
                 <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
                 <img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
               </div>
-              <div className="hidden sm:block sm:ml-6">
-                <div className="flex space-x-4">
-                  <Link to="/signup" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign up</Link>
-                  <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in</Link>
-                </div>
-              </div>
-            </div>
-
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              {/* <!-- Profile dropdown --> */}
-              <div className="ml-3 relative">
-                {/* Dopdown menu button (user profile picture) */}
-                <div>
-                  <button onClick={this.showModal} type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-expanded="false" aria-haspopup="true">
-                    <span className="sr-only">Open user menu</span>
-                    <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                  </button>
-                </div>
-
-                {/* Dropdown menu, show/hide based on menu state. */}
-                {/* Entering: "transition ease-out duration-100" */}
-                {/* From: "transform opacity-0 scale-95" */}
-                {/* To: "transform opacity-100 scale-100" */}
-                {/* Leaving: "transition ease-in duration-75" */}
-                {/* From: "transform opacity-100 scale-100" */}
-                {/* To: "transform opacity-0 scale-95" */}
-
-                {/* Actual profile dropdown menu */}
-                {this.state.showModal &&
-                  <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
+              {/* Log in/Sign up link group, shown when there is no logged in user */}
+              {this.state.user === null &&
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    <Link to="/signup" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign up</Link>
+                    <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log in</Link>
                   </div>
-                }
-              </div>
-
+                </div>
+              }
             </div>
+
+            {/* User profile section, state: logged in */}
+            {this.state.user != null &&
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* <!-- Profile dropdown --> */}
+                <div className="ml-3 relative">
+                  {/* Dopdown menu button (user profile picture) */}
+                  <div>
+                    <button onClick={this.showModal} type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-expanded="false" aria-haspopup="true">
+                      <span className="sr-only">Open user menu</span>
+                      <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    </button>
+                  </div>
+
+                  {/* Dropdown menu, show/hide based on menu state. */}
+                  {/* Entering: "transition ease-out duration-100" */}
+                  {/* From: "transform opacity-0 scale-95" */}
+                  {/* To: "transform opacity-100 scale-100" */}
+                  {/* Leaving: "transition ease-in duration-75" */}
+                  {/* From: "transform opacity-100 scale-100" */}
+                  {/* To: "transform opacity-0 scale-95" */}
+
+                  {/* Actual profile dropdown menu */}
+                  {this.state.showModal &&
+                    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
+                    </div>
+                  }
+                </div>
+              </div>
+            }
+
           </div>
         </div>
 
         {/* <!-- Mobile menu, show/hide based on menu state. --> */}
         <div className="sm:hidden" id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1">
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+            <Link to="/signup" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign up</Link>
+            <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Log in</Link>
           </div>
         </div>
       </nav>
