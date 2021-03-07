016e568f4a5a6b00a0def9622a1b3c618b53bd98
pr1m0x <kraemer.timo1@gmail.com>
2021-03-07 16:01:23 +0100

    finished event form and updated event model

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Home from './components/Home';
+import CreateEvent from './components/CreateEvent';
 import { Route } from 'react-router-dom';
-
+import Navbar from './components/NavBar';
 class App extends React.Component {
   state = {
     user: this.props.user,
@@ -19,9 +20,11 @@ class App extends React.Component {
   render() {
     return (
       <>
+        <Navbar />
         <Route exact path="/" render={Home} />
         <Route exact path="/login" render={(props) => <Login setUser={this.setUser} {...props} />} />
         <Route exact path="/signup" render={(props) => <Signup setUser={this.setUser} {...props} />} />
+        <Route exact path="/start" render={(props) => <CreateEvent creator={this.state.user._id} {...props} />} />
       </>
     );
   }
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { uploader } = require('../cloudinary.config');
 const eventController = require('../controllers/eventController');
 
 router.get('/', eventController.getAllEvents);
-router.post('/', eventController.createEvent);
+router.post('/', uploader.single('banner'), eventController.createEvent);
 router.get('/:id', eventController.getEventDetails);
 router.put('/:id', eventController.updateEvent);
 router.delete('/:id', eventController.deleteEvent);
