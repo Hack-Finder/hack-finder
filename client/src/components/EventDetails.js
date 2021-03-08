39f3c9ad909dabe15a7f0e7c506800e944cbdc90
Florian Poschlod <florian.poschlod@mailbox.org>
2021-03-08 16:17:32 +0100

    EventsDetails, wip...

--- client/src/components/EventDetails.js
+++ client/src/components/EventDetails.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { getEventDetails } from '../services/event';
 
-export default function EventDetails() {
+export default function EventDetails(props) {
+
+  const eventId = props.match.params.id;
+  // let event = {};
+
+  // getEventDetails(eventId).then(eventFromDb => {
+  //   console.log(eventFromDb)
+  //   return eventFromDb
+  // });
+
+  // const event = getEventDetails(eventId).then(eventFromDb => {
+  //   console.log(eventFromDb)
+  //   // event = eventFromDb
+  //   return eventFromDb
+  // }).catch(err => {
+  //   console.log(err)
+  // });
+
+  // console.log('event', event)
+
   return (
     <div>
       <h2>Event Details</h2>
     </div>
   )
 }
+
+// import React from 'react'
+
+// export default class EventDetails extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>event details</h2>
+//       </div>
+//     )
+//   }
+// }
+
 
