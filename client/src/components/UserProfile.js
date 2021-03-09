e89dedafe3e4d73a209c0e9273806a621d788c95
Florian Poschlod <florian.poschlod@mailbox.org>
2021-03-09 17:01:55 +0100

    Removes commented out code

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,26 +1,14 @@
-// import React from 'react'
-
-// export default function UserProfile(props) {
-
-//   return (
-//     <div>
-//       <div>
-//         <img src={props.user.username} alt="User" />
-//         <h2>Hello, {props.user.username}!</h2>
-//         <p>{props.user.email}</p>
-//         {/* <p>{props.user.description}</p> */}
-//       </div>
-
-//       <div>
-//         <h2>Your events</h2>
-//       </div>
-//     </div>
-//   )
-// }
-
 import React, { Component } from 'react'
 
 export default class UserProfile extends Component {
+
+  state = {
+    events: null
+  }
+
+  componentDidMount() {
+    
+  }
 
   render() {
   
