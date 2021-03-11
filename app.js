0aa759cdabc359aa7fe3400036d7debf9002e36d
pr1m0x <kraemer.timo1@gmail.com>
2021-03-11 21:33:39 +0100

    commented error handler out

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
@@ -97,11 +97,11 @@ app.use('/api/users', userRoutes);
 app.use('/api/events', eventRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+// require('./error-handling')(app);
 
 app.use((req, res) => {
   // If no routes match, send them the React HTML.
-  res.sendFile(__dirname + "/client/build/index.html");
+  res.sendFile(__dirname + '/client/build/index.html');
 });
 
 module.exports = app;
